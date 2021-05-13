99bb6764726798a493368b82ac72d341a67979b8
martyewings <martyewings@gmail.com>
2021-05-13 15:37:16 +0100

    adding in error handling

--- tasks/st0370_generate_token.sh
+++ tasks/st0370_generate_token.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+declare PT__installdir
+# shellcheck disable=SC1090
+source "$PT__installdir/support_tasks/files/common.sh"
 # Generate Token for requested user, for consumption in other tasks
 # shellcheck disable=SC2086
-declare PT_user
-declare PT_password
-password=$PT_password
-user=$PT_user
+declare user
+declare password
 
 
-printf "%s" "$password" | puppet access login $user --lifetime 1d
+printf "%s" "$password" | puppet access login $user --lifetime 1d || fail "unable to generate token"
+
+success '{ "status": "Token for $user created" }'
--- tasks/st0373_api_calls.sh
+++ tasks/st0373_api_calls.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
-
-#
+declare PT__installdir
+# shellcheck disable=SC1090
+source "$PT__installdir/support_tasks/files/common.sh"
 # shellcheck disable=SC2046
 
-declare PT_command
-command=$PT_command
+delare $command
 
 
 
 if [ -e "/etc/sysconfig/pe-puppetserver" ] || [ -e "/etc/default/pe-puppetserver" ] # Test to confirm this is a Puppetserver
 then
-  echo "Puppet master node detected"   #Log Line to StdOut for the Console
-
 
 case $command in
      create_role_cd4pe)
-          curl -X POST -H 'Content-Type: application/json' --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://$(hostname -f):4433/rbac-api/v1/roles -d '{"description":"CD4PE user role","display_name":"CD4PE Role","user_ids":[],"group_ids":[],"permissions":[{"object_type":"node_groups","action":"modify_children","instance":"*"},{"object_type":"node_groups","action":"set_environment","instance":"*"},{"object_type":"node_groups","action":"view","instance":"*"},{"object_type":"puppet_agent","action":"run","instance":"*"},{"object_type":"environment","action":"deploy_code","instance":"*"},{"object_type":"nodes","action":"view_data","instance":"*"},{"object_type":"node_groups","action":"edit_config_data","instance":"*"},{"object_type":"orchestrator","action":"view","instance":"*"}]}'
+          curl -X POST -H 'Content-Type: application/json' --cert $(puppet config print hostcert) --key $(puppet config print hostprivkey) --cacert $(puppet config print localcacert) https://$(hostname -f):4433/rbac-api/v1/roles -d '{"description":"CD4PE user role","display_name":"CD4PE Role","user_ids":[],"group_ids":[],"permissions":[{"object_type":"node_groups","action":"modify_children","instance":"*"},{"object_type":"node_groups","action":"set_environment","instance":"*"},{"object_type":"node_groups","action":"view","instance":"*"},{"object_type":"puppet_agent","action":"run","instance":"*"},{"object_type":"environment","action":"deploy_code","instance":"*"},{"object_type":"nodes","action":"view_data","instance":"*"},{"object_type":"node_groups","action":"edit_config_data","instance":"*"},{"object_type":"orchestrator","action":"view","instance":"*"}]}' || fail "could not create role"
           ;;
        list_tokens)
-          su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d pe-rbac -c \"select subjects.login,tokens.expiration FROM subjects LEFT JOIN tokens ON subjects.id = tokens.user_id\"" 
+          su - pe-postgres -s /bin/bash -c "/opt/puppetlabs/server/bin/psql -d pe-rbac -c \"select subjects.login,tokens.expiration FROM subjects LEFT JOIN tokens ON subjects.id = tokens.user_id\"" || fail "could not list tokens"
           ;;
   manual_gitlab_webhook_hit)
 	  #needs detection of token
-          curl -k -v -X POST -H "Content-Type: application/json" "https://$(hostname -f):8170/code-manager/v1/webhook?type=gitlab&token=$(cat ~/.puppetlabs/token)" -d '{ "ref": "refs/heads/production" }'
+          curl -k -v -X POST -H "Content-Type: application/json" "https://$(hostname -f):8170/code-manager/v1/webhook?type=gitlab&token=$(cat ~/.puppetlabs/token)" -d '{ "ref": "refs/heads/production" }' || fail "unable to trigger webhook"
           ;;
      get_all_services_status)
           SET_SERVER=$(puppet config print server)
@@ -33,11 +31,11 @@ case $command in
  	 --cert   $(puppet config print hostcert) \
   	--key    $(puppet config print hostprivkey) \
   	--cacert $(puppet config print localcacert) \
-  	https://"${CONSOLE}":4433/status/v1/services | python -m json.tool
+  	https://"${CONSOLE}":4433/status/v1/services | python -m json.tool || fail "unable to get service status"
 	  ;;
 esac
 else
-  echo  "Not a Puppet master node, exiting"
+  fail  "Not a Puppet master node, exiting"
 
 fi
 
